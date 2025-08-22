9dc5235927079136af15ffb5539831cd5d22116e
mateusaraujo1 <f.mateusaraujo2@gmail.com>
2025-08-22 09:12:41 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,10 @@
+import express from 'express'
+
+const app = express()
+
+// digo o método http, a rota, request e response
+app.get('/usuarios', (req, res) => {
+    res.send('Hello World')
+})
+
+app.listen(3000) // porta que o servidor vai rodar
